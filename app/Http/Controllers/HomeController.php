db849f27a6f574942330bdc6a5124b56e2fa1fe8
ghrtmb <goharparna@gmail.com>
2024-07-20 19:56:20 +0700

    Edit - Menambah tampilan home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guru;
+use App\Models\Hari_model;
+use App\Models\Jadwal;
+use App\Models\Pelajaran_model;
+use App\Models\Pengumuman_model;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +14,29 @@ class HomeController extends Controller
     public function index()
     {
         $page_title = 'Home';
-        return view('home', compact('page_title'));
+
+        $data = [
+            [
+                'nama' => 'Hari',
+                'jumlah' => Hari_model::count(),
+            ],
+            [
+                'nama' => 'Guru',
+                'jumlah' => Guru::count(),
+            ],
+            [
+                'nama' => 'Pelajaran',
+                'jumlah' => Pelajaran_model::count(),
+            ],
+            [
+                'nama' => 'Jadwal',
+                'jumlah' => Jadwal::count(),
+            ],
+            [
+                'nama' => 'Pengumuman',
+                'jumlah' => Pengumuman_model::count(),
+            ],
+        ];
+        return view('home', compact('page_title', 'data'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,23 @@
 @section('content')
     <div class="card">
         <div class="card-header bg-success-subtle">
-            <h3>{{ $page_title }}</h3>
+            <h3>{{ $page_title }} Summary</h3>
         </div>
         <div class="card-body">
-            Tampilan Home
+            <div class="row">
+                @foreach ($data as $row)
+                    <div class="col-2" title="Total data dari {{ $row['nama'] }}">
+                        <div class="card text-center">
+                            <div class="card-header bg-success">
+                                <span class="text-bg-success">{{ $row['nama'] }}</span>
+                            </div>
+                            <div class="card-body bg-success-subtle">
+                                <h1>{{ $row['jumlah'] }}</h1>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
